d4ede4dbb12d9db7c6e4e14d4456edf582cc0c0c
HaJunHyeok <pawn@boxbreakers.co>
2016-11-04 22:30:16 +0900

    Initial commit

---
+++ Pods/Target Support Files/Pods-Una_iOSUITests/Pods-Una_iOSUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Una_iOSUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Una_iOSUITestsVersionString[];
+
---
+++ Una_iOSTests/Una_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Una_iOSTests.swift
+//  Una_iOSTests
+//
+//  Created by 하준혁 on 2016. 11. 4..
+//  Copyright © 2016년 하준혁(Enoxaiming). All rights reserved.
+//
+
+import XCTest
+@testable import Una_iOS
+
+class Una_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
